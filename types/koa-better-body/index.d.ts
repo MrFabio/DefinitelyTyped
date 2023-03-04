0cb2dd8fa3abe42e0ad116a0dd1e032e8e39c6a9
Fábio Gomes <MrFabio@users.noreply.github.com>
2023-03-04 01:26:27 +0000

    Updated koa-better-body types

--- types/koa-better-body/index.d.ts
+++ types/koa-better-body/index.d.ts
@@ -77,6 +77,56 @@ declare namespace KoaBetterBody {
     bufferLimit?: string;
 
     /**
+     * limit the amount of uploaded files, set Infinity for unlimited
+     *
+     * @default Infinity
+     */
+    maxFiles?: number | undefined;
+
+    /**
+     * the minium size of uploaded file
+     *
+     * @default 1
+     */
+    minFileSize?: number | undefined;
+
+    /**
+     * limit the size of uploaded file
+     *
+     * @default 200 * 1024 * 1024
+     */
+    maxFileSize?: number | undefined;
+
+    /**
+     * limit the size of the batch of uploaded files
+     *
+     * @default options.maxFileSize
+     */
+    maxTotalFileSize?: number | undefined;
+
+    /**
+     * limit the number of fields, set 0 for unlimited
+     *
+     * @default 1000
+     */
+    maxFields?: number | undefined;
+
+    /**
+     * limit the amount of memory all fields together (except files) can allocate in bytes
+     *
+     * @default 20 * 1024 * 1024
+     */
+    maxFieldsSize?: number | undefined;
+
+    /**
+     * include checksums calculated for incoming files, set this to some hash algorithm, see
+     * crypto.createHash for available algorithms
+     *
+     * @default false
+     */
+    hashAlgorithm?: string | false | undefined;
+
+    /**
      * @default false
      */
     buffer?: boolean;
